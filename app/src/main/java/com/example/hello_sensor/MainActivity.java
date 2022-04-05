ded0fc839e59f953dc9f171082068b87f466114e
drawdehe <edward.sjoblom@gmail.com>
2022-04-05 11:51:30 +0200

    First commit

---
+++ app/src/main/java/com/example/hello_sensor/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hello_sensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// check https://www.youtube.com/watch?v=IzzGVLnZBfQ&ab_channel=SarthiTechnology
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
